f81cfd8b724c4daa93c19d5035e752564159cdfa
Vishalgupta7518 <vishalgupta916931@gmail.com>
2024-08-13 10:28:34 +0530

    My new File

---
+++ hello.php
@@ -0,0 +1,28 @@
+<?php
+
+$str ="Delhi,Gurgaon,Mumbai";
+// Maths,Science,English,History
+
+// $data =  explode(',',$str);
+$data =  str_split($str,2);
+
+print_r($data);
+// echo $data['1'];
+
+
+// move_uploaded_file($_FILES['file']['tmp'],'uploads/'.$_FILES['file']['name']);
+
+
+
+
+?>
+
+<!-- name  rollno  marks
+abc   12      400
+xyz   14      450
+xyz   14      450
+tva   11      280 -->
+
+
+
+
